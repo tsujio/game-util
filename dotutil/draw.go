99d64f29336f9afe7c5b1ef3919ea4f124edd370
tsujio <ntsujio@gmail.com>
2021-09-10 12:58:42 +0900

    Update DrawImage

--- dotutil/draw.go
+++ dotutil/draw.go
@@ -14,26 +14,30 @@ const (
 )
 
 type DrawImageOption struct {
-	Scale        float64
-	Rotate       float64
-	BasePosition DrawImagePosition
+	Scale          float64
+	ScaleX, ScaleY float64
+	Rotate         float64
+	BasePosition   DrawImagePosition
 }
 
 func DrawImage(dst *ebiten.Image, img *ebiten.Image, x, y float64, opt *DrawImageOption) {
 	o := &ebiten.DrawImageOptions{}
-	if opt.Scale != 0 {
-		o.GeoM.Scale(opt.Scale, opt.Scale)
-	}
-	o.GeoM.Rotate(opt.Rotate)
-	o.GeoM.Translate(x, y)
 
 	switch opt.BasePosition {
 	case DrawImagePositionCenter:
 		w, h := img.Size()
-		r := math.Sqrt(math.Pow(float64(w)*opt.Scale, 2)+math.Pow(float64(h)*opt.Scale, 2)) / 2
+		r := math.Sqrt(math.Pow(float64(w), 2)+math.Pow(float64(h), 2)) / 2
 		rad := math.Atan2(float64(h), float64(w))
-		o.GeoM.Translate(-r*math.Cos(opt.Rotate+rad), -r*math.Sin(opt.Rotate+rad))
+		o.GeoM.Translate(-r*math.Cos(rad), -r*math.Sin(rad))
 	}
+
+	if opt.ScaleX != 0 || opt.ScaleY != 0 {
+		o.GeoM.Scale(opt.ScaleX, opt.ScaleY)
+	} else if opt.Scale != 0 {
+		o.GeoM.Scale(opt.Scale, opt.Scale)
+	}
+	o.GeoM.Rotate(opt.Rotate)
+	o.GeoM.Translate(x, y)
 
 	dst.DrawImage(img, o)
 }
