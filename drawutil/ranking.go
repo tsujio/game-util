c64d26d276c2d75278f6e06c271bcaa5630276d9
tsujio <ntsujio@gmail.com>
2023-01-08 16:09:08 +0900

    Add DrawRanking

---
+++ drawutil/ranking.go
@@ -0,0 +1,107 @@
+package drawutil
+
+import (
+	"fmt"
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"github.com/tsujio/game-logging-server/client"
+	"github.com/tsujio/game-util/resourceutil"
+)
+
+const (
+	defaultScreenWidth  = 640
+	defaultScreenHeight = 480
+)
+
+type DrawRankingOption struct {
+	BackgroundColor, FontColor color.Color
+	TitleFont, BodyFont        *resourceutil.Font
+	ScreenWidth, ScreenHeight  int
+}
+
+func DrawRanking(dst *ebiten.Image, ranking []client.GameScore, playerID string, opt *DrawRankingOption) {
+	if opt == nil {
+		opt = &DrawRankingOption{}
+	}
+	if opt.BackgroundColor == nil {
+		opt.BackgroundColor = color.RGBA{0, 0, 0, 0xa0}
+	}
+	if opt.FontColor == nil {
+		opt.FontColor = color.White
+	}
+	if opt.ScreenWidth == 0 {
+		opt.ScreenWidth = defaultScreenWidth
+	}
+	if opt.ScreenHeight == 0 {
+		opt.ScreenHeight = defaultScreenHeight
+	}
+
+	ebitenutil.DrawRect(
+		dst,
+		20,
+		40,
+		float64(opt.ScreenWidth)-20*2,
+		float64(opt.ScreenHeight)-40*2,
+		opt.BackgroundColor,
+	)
+
+	rankingText := "RANKING"
+	text.Draw(
+		dst,
+		rankingText,
+		opt.TitleFont.Face,
+		opt.ScreenWidth/2-len(rankingText)*int(opt.TitleFont.FaceOptions.Size)/2,
+		110,
+		opt.FontColor,
+	)
+
+	ranking = ranking[:5]
+
+	rankIn := false
+	for _, r := range ranking {
+		if r.PlayerID == playerID {
+			rankIn = true
+		}
+	}
+
+	hText := "   SCORE    DATE   "
+	if rankIn {
+		hText += "     "
+	}
+	text.Draw(
+		dst,
+		hText,
+		opt.BodyFont.Face,
+		opt.ScreenWidth/2-len(hText)*int(opt.BodyFont.FaceOptions.Size)/2,
+		170,
+		opt.FontColor,
+	)
+
+	for i, r := range ranking {
+		rank := 1
+		for _, s := range ranking[:i] {
+			if r.Score < s.Score {
+				rank++
+			}
+		}
+		t := fmt.Sprintf("%d. %5d %s", rank, r.Score, r.Timestamp.Local().Format("2006.01.02"))
+		if rankIn {
+			if r.PlayerID == playerID {
+				t += " YOU!"
+			} else {
+				t += "     "
+			}
+		}
+		text.Draw(
+			dst,
+			t,
+			opt.BodyFont.Face,
+			opt.ScreenWidth/2-len(t)*int(opt.BodyFont.FaceOptions.Size)/2,
+			170+(i+1)*int(opt.BodyFont.FaceOptions.Size*2),
+			opt.FontColor,
+		)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,16 +3,22 @@ module github.com/tsujio/game-util
 go 1.19
 
 require (
+	github.com/hajimehoshi/ebiten/v2 v2.4.15
+	github.com/tsujio/game-logging-server/client v0.0.0-20230108051530-a7e4a1b23b63
+	golang.org/x/image v0.2.0
+)
+
+require (
 	github.com/ebitengine/purego v0.1.1 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
-	github.com/gofrs/flock v0.8.0 // indirect
-	github.com/hajimehoshi/ebiten/v2 v2.4.15
 	github.com/hajimehoshi/file2byteslice v1.0.0 // indirect
-	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.3 // indirect
+	github.com/hajimehoshi/oto/v2 v2.3.1 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
+	github.com/jfreymuth/oggvorbis v1.0.4 // indirect
+	github.com/jfreymuth/vorbis v1.0.2 // indirect
 	golang.org/x/exp/shiny v0.0.0-20221230185412-738e83a70c30 // indirect
-	golang.org/x/image v0.2.0
 	golang.org/x/mobile v0.0.0-20221110043201-43a038452099 // indirect
 	golang.org/x/sys v0.3.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	golang.org/x/text v0.5.0 // indirect
 )
